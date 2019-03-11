79f08191c7677354f595c4c70c3b122071faae4c
David Klein <david.klein@psmtec.de>
2019-03-11 11:42:51 +0100

    use tabs

--- src/kex/UserPaths.hx
+++ src/kex/UserPaths.hx
@@ -1,18 +1,18 @@
 package kex;
 
 class UserPaths {
-    public static function homeDirectory() : String {
-// TODO (DK) why is this `!macro` crap neede?
+	public static function homeDirectory() : String {
+// TODO (DK) why is this `!macro` crap needed?
 #if !macro
  #if kha_debug_html5
-        return js.Syntax.code("require('electron').remote.require('os').homedir()");
+		return js.Syntax.code("require('electron').remote.require('os').homedir()");
  #elseif (kha_linux || kha_macos)
-        return Sys.getEnv('HOME');
+		return Sys.getEnv('HOME');
  #else
-        return Sys.getEnv('HOMEPATH');
+		return Sys.getEnv('HOMEPATH');
  #end
 #else
-        return '';
+		return '';
 #end
     }
 }
