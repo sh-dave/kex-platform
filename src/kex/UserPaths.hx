066971d2ac1911b5e15ffc717548bcc0c776c599
David Klein <david.klein@psmtec.de>
2019-03-11 11:37:58 +0100

    initial

---
+++ src/kex/UserPaths.hx
@@ -0,0 +1,18 @@
+package kex;
+
+class UserPaths {
+    public static function homeDirectory() : String {
+// TODO (DK) why is this `!macro` crap neede?
+#if !macro
+ #if kha_debug_html5
+        return js.Syntax.code("require('electron').remote.require('os').homedir()");
+ #elseif (kha_linux || kha_macos)
+        return Sys.getEnv('HOME');
+ #else
+        return Sys.getEnv('HOMEPATH');
+ #end
+#else
+        return '';
+#end
+    }
+}
